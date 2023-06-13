ff4fd2aeb489eacc5d4282cb3b488f4382d865b1
Joep van Dam <joep.vandam@student.hu.nl>
2023-06-13 15:51:04 +0200

    Introlevel tutorial aangepast aan nieuwe puzzel

--- Assets/Scripts/Tutorials/InteractTutorial.cs
+++ Assets/Scripts/Tutorials/InteractTutorial.cs
@@ -4,16 +4,23 @@ public class InteractTutorial : MonoBehaviour
 {
     [SerializeField] private GameObject InteractPopUp;
     [SerializeField] private GameObject NextTutorial;
+    [SerializeField] private int PuzzleInt = 1;
 
     void Start()
     {
         InteractPopUp.gameObject.SetActive(true);
     }
 
-    /*private void Update()
+    private void Update()
     {
-        InteractPopUp.gameObject.SetActive(false);
-        NextTutorial.gameObject.SetActive(true);
-        enabled = false;
-    }*/
+        if (PlayerPrefs.HasKey("Puzzle" + PuzzleInt + "Completed"))
+        {
+            if (PlayerPrefs.GetInt("Puzzle" + PuzzleInt + "Completed") == 1)
+            {
+                InteractPopUp.gameObject.SetActive(false);
+                NextTutorial.gameObject.SetActive(true);
+                enabled = false;
+            }
+        }
+    }
 }
