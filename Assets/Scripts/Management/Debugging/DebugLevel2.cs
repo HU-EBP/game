95e89654939be7bc374f09ff7c7ef8aa1a62335f
Joep van Dam <joep.vandam@student.hu.nl>
2023-06-13 21:12:56 +0200

    Meer debug screen/opties verbeteringen

--- Assets/Scripts/Management/Debugging/DebugLevel2.cs
+++ Assets/Scripts/Management/Debugging/DebugLevel2.cs
@@ -5,9 +5,15 @@ public class DebugLevel2 : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI ButtonText;
     [SerializeField] private bool IsCompleteButton = false;
+    [SerializeField] private bool IsCheckpointButton = false;
     private bool Level2 = false;
+    private bool Level2CP = false;
 
-    private void Start() { if (IsCompleteButton) { SetButtonText(); } }
+    private void Start()
+    {
+        if (IsCompleteButton) { SetCompleteText(); }
+        if (IsCheckpointButton) { SetCheckpointText(); }
+    }
 
     // Function to change the completion status of level 2
     public void ChangeCompleteState()
@@ -18,23 +24,23 @@ public class DebugLevel2 : MonoBehaviour
             if (Level2)
             {
                 PlayerPrefs.SetInt("Level2Complete", 0);
-                SetButtonText();
+                SetCompleteText();
             }
             else if (!Level2)
             {
                 PlayerPrefs.SetInt("Level2Complete", 1);
-                SetButtonText();
+                SetCompleteText();
             }
         }
         else
         {
             PlayerPrefs.SetInt("Level2Complete", 1);
-            SetButtonText();
+            SetCompleteText();
         }
     }
 
     // Function to change button text of a completion button
-    private void SetButtonText()
+    private void SetCompleteText()
     {
         if (PlayerPrefs.HasKey("Level2Complete"))
         {
@@ -43,6 +49,42 @@ public class DebugLevel2 : MonoBehaviour
             else if (!Level2) { ButtonText.text = "Level 2 gehaald: FALSE"; }
         }
         else { ButtonText.text = "Level 2 gehaald: FALSE"; }
+    }
+
+    // Function to change the status of the level 2 checkpoint
+    public void ChangeCheckpointState()
+    {
+        if (PlayerPrefs.HasKey("Checkpoint1"))
+        {
+            Level2CP = PlayerPrefs.GetInt("Checkpoint1") == 1 ? true : false;
+            if (Level2CP)
+            {
+                PlayerPrefs.SetInt("Checkpoint1", 0);
+                SetCheckpointText();
+            }
+            else if (!Level2CP)
+            {
+                PlayerPrefs.SetInt("Checkpoint1", 1);
+                SetCheckpointText();
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Checkpoint1", 1);
+            SetCheckpointText();
+        }
+    }
+
+    // Function to change button text of a checkpoint button
+    private void SetCheckpointText()
+    {
+        if (PlayerPrefs.HasKey("Checkpoint1"))
+        {
+            Level2CP = PlayerPrefs.GetInt("Checkpoint1") == 1 ? true : false;
+            if (Level2CP) { ButtonText.text = "Level 2 checkpoint: TRUE"; }
+            else if (!Level2CP) { ButtonText.text = "Level 2 checkpoint: FALSE"; }
+        }
+        else { ButtonText.text = "Level 2 checkpoint: FALSE"; }
     }
 
     // Function to hard reset level 2
